7230edec918f9e4644149608f8e3f9f5754298ea
isra-elwanda-putra <61801037+isra-elwanda-putra@users.noreply.github.com>
2023-07-13 07:49:48 +0700

    Update and rename note.js to Note.js

--- routes/Note.js
+++ routes/Note.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Note = require('../models/note.js');
+const Note = require('../models/Note.js');
 
 router.post("/list", async function (req, res) {
     var notes = await Note.find({ userid: req.body.userid });

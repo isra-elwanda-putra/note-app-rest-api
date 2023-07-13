cdba8aaacd96c63924515abb4cc3177951f3c68f
isra-elwanda-putra <61801037+isra-elwanda-putra@users.noreply.github.com>
2023-07-13 07:40:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-const Note = require('./models/Note');
+const Note = require('./models/note');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
